3c2bf40ebc8b6c3cd12e686e99443a6f4a676911
Prakash Kumar <prakash@virtusa.com>
2018-05-31 18:36:59 +0530

    modified third time

--- JS/calculator.js
+++ JS/calculator.js
@@ -21,30 +21,46 @@ function getSecondInput(inp2)
 function add()
 {
 input1=display;
+display+="+";
+document.getElementById('expression').value=display;
 }
 function subtract()
 {
 input1=display;
+display+="-";
+document.getElementById('expression').value=display;
 }
 function multiply()
 {
-input1=display;
+input1=parseInt(display);
+display+="*";
+document.getElementById('expression').value=display;
 }
 function divide()
 {
 input1=display;
+display+="/";
+document.getElementById('expression').value=display;
 }
 function displayResult()
 {
+var split=document.getElementById('expression').value;
+var splitaft=split.split('+')[2];
+console.log(input1);
+console.log(splitaft);
 
+//var input2=parseInt(split[2]);
+//display+=split[2];
+document.getElementById('answer')=input2;
 }
 function clearinputbox()
 {
-document.getElementById('inputbox').value="";
+document.getElementById('expression').value="";
+display="";
 }
 function displayInput(disp)
 {
     display+=disp.toString();
-    document.getElementById('inputbox').value=display;
+    document.getElementById('expression').value=display;
 
 }
