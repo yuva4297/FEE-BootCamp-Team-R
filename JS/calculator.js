65d77a49b3c1c2a1fd67d6f05d3ccf60095a3a8b
Prakash Kumar <prakash@virtusa.com>
2018-06-01 09:33:21 +0530

    Basic MouseEvent Operations are done

--- JS/calculator.js
+++ JS/calculator.js
@@ -1,14 +1,13 @@
 
 var result = 0;
 var expression = "";
-var operator = null;
+var operator = "";
 function calculate()
 {
     document.getElementById('answer').value = "HELLO";
 }
-var res;
 var input1;
-var input2;
+var input2=[];
 var display="";
 function getFirstInput(inp1)
 {
@@ -20,42 +19,124 @@ function getSecondInput(inp2)
 }
 function add()
 {
+    if(display!="")
+    {
+operator+="+";
 input1=display;
 display+="+";
 document.getElementById('expression').value=display;
+    }
+    else{
+        alert("please enter any numbers before pressing operator");
+    }
 }
 function subtract()
 {
+    if(display!="")
+    {
+operator+="-";
 input1=display;
 display+="-";
 document.getElementById('expression').value=display;
+    }
+    else{
+        alert("please enter any numbers before pressing operator");
+    }
 }
 function multiply()
 {
-input1=parseInt(display);
+    if(display!="")
+    {
+operator+="*";
+input1=display;
 display+="*";
 document.getElementById('expression').value=display;
+    }
+    else{
+        alert("please enter any numbers before pressing operator");
+    }
 }
 function divide()
 {
+    if(display!="")
+    {
+operator+="/"
 input1=display;
 display+="/";
 document.getElementById('expression').value=display;
+    }
+    else{
+        alert("please enter any numbers before pressing operator");
+    }
+}
+function modulo()
+{
+    if(display!="")
+    {
+operator+="%";
+input1=display;
+display+="%";
+document.getElementById('expression').value=display;
+    }
+    else{
+        alert("please enter any numbers before pressing operator");
+    }
+}
+function decimal()
+{
+    display+=".";
+    document.getElementById('expression').value=display;
 }
 function displayResult()
 {
-var split=document.getElementById('expression').value;
-var splitaft=split.split('+')[2];
-console.log(input1);
-console.log(splitaft);
+    if(operator.length==1)
+    switch(operator)
+    {
+    case "+":
+            var split=document.getElementById('expression').value;
+            input2=split.split("+");
+            result=Number(input1)+Number(input2[1]);
+            document.getElementById('answer').value=result;
+            break;
+    case "-":
+            var split=document.getElementById('expression').value;
+            input2=split.split("-");
+            result=Number(input1)-Number(input2[1]);
+            document.getElementById('answer').value=result;
+            break;
+    case "*":
+            var split=document.getElementById('expression').value;
+            input2=split.split("*");
+            result=Number(input1)*Number(input2[1]);
+            document.getElementById('answer').value=result;
+            break;
+     case "/":
+            var split=document.getElementById('expression').value;
+            input2=split.split("/");
+            result=Number(input1)/Number(input2[1]);
+            document.getElementById('answer').value=result;
+            break;
+    case "%":
+            var split=document.getElementById('expression').value;
+            input2=split.split("%");
+            result=Number(input1)%Number(input2[1]);
+            document.getElementById('answer').value=result;
+            break;
+    default:
+            break;
 
-//var input2=parseInt(split[2]);
-//display+=split[2];
-document.getElementById('answer')=input2;
+    }
+    else{
+        alert("invalid expression");
+        document.getElementById('expression').value="";
+        document.getElementById('answer').value="";
+        display="";
+    }
 }
 function clearinputbox()
 {
 document.getElementById('expression').value="";
+document.getElementById('answer').value="";
 display="";
 }
 function displayInput(disp)
