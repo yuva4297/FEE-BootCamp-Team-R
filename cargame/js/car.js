cf3a3323236c465c7c1fbc14277175c318ab131f
Yuvarani Ramu <yuvaranir@virtusa.com>
2018-06-01 18:50:38 +0530

    Dummy changes

---
+++ cargame/js/car.js
@@ -0,0 +1,10 @@
+document.addEventListener("keydown", function(event) {
+   
+    console.log("Hello"+event);
+    findTheDirectionPressed(event);
+    findTheDirectionPressed(event)
+{
+    console.log(event.which);
+}
+});
+
